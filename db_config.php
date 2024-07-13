739a0f81ea3a56b2c0b9daf34d993bee2b61ca63
amnishittechxpert <amnishittechxpert@gmail.com>
2024-07-13 19:15:08 +0530

    older code

--- db_config.php
+++ db_config.php
@@ -1,15 +1,15 @@
 <?php
-$servername = "localhost"; // Replace with your server name
-$username = "root"; // Replace with your database username
-$password = ""; // Replace with your database password
-$dbname = "user"; // Replace with your database name
+$servername = "localhost"; 
+$username = "root"; 
+$password = ""; 
+$dbname = "user";
 
-// Create connection
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "amnnnnnsh";
+
 ?>
